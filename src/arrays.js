13e8fb1d964a41346bb7419293e14f4c52e534cd
midoghranek <midoghranek@hotmail.com>
2021-11-18 09:51:21 +0200

    📦 NEW: add toggleArrayValue function

--- src/arrays.js
+++ src/arrays.js
@@ -7,6 +7,18 @@
  */
 const containsAll = (baseArr, arr) => arr.every((ele) => baseArr.includes(ele));
 
+/**
+ * Check that every element in an array exists in the other array.
+ * @param {array} array The array to toggle value from.
+ * @param {value} value The value to be toggled from array.
+ */
+const toggleArrayValue = (array, value) => {
+  return array.includes(value)
+    ? [...array.filter((i) => i !== value)]
+    : [...array, value];
+};
+
 module.exports = {
   containsAll,
+  toggleArrayValue,
 };
