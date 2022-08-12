16c9657583fc8767a775a427b5d355ffe62be680
Youssef Zidan <yzaidan@aas.com.sa>
2022-08-12 19:25:47 +0200

    exporting naming functions

---
+++ index.js
@@ -0,0 +1,7 @@
+export * from "./src/arrays.js";
+export * from "./src/dateAndTime.js";
+export * from "./src/forms.js";
+export * from "./src/numbers.js";
+export * from "./src/objects.js";
+export * from "./src/strings.js";
+export * from "./src/window.js";
