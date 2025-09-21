f9ee8e4176b678d67d276526d4046dc5d8ad44ac
youssefzidan <youssefzidan2200@gmail.com>
2025-09-21 14:32:03 +0300

    add TypeScript declaration file to export modules from src

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export * from "./src/arrays";
+export * from "./src/dateAndTime";
+export * from "./src/forms";
+export * from "./src/numbers";
+export * from "./src/objects";
+export * from "./src/strings";
+export * from "./src/window";
